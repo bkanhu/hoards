7043e1451c86b0f2fd52c9b1409275fb4aa379a8
B Kanhu Charan <iamkanhuc@gmail.com>
2023-08-17 22:16:35 +0530

    modified:   pages/dashboard.js

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -107,7 +107,6 @@ const Dashboard = () => {
                 <div
                   key={hoarding.id}
                   className="p-4 bg-white rounded-lg shadow-lg"
-                  onClick={() => viewTheHoardingModal(hoarding.id)}
                 >
                   <h2 className="text-lg font-semibold">{hoarding.note}</h2>
                   <p className="text-sm text-gray-500">{hoarding.location}</p>
